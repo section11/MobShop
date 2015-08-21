2286fe11f34b29d75e25059167590e52e000025d
section11 <csegarceanu@gmail.com>
2015-08-21 16:19:55 -0500

    Web Panel

---
+++ Web Database Management/application/views/admin_panel.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+	<title>Admin | MobShop</title>
+    <link rel="stylesheet" href="<?php echo base_url()?>css/style.css"> <!-- Main stylesheet -->
+	<link rel="stylesheet" href="<?php echo base_url()?>css/style-responsive.css"> <!-- Main stylesheet responsive -->
+</head>
+<body background="<? echo base_url()?>images/background.jpg">
+	<div class="page-wrap" >
+   
+	</div>
+	
+    <footer class="site-footer">
+      <div class="navigation">
+		<ul class="nav hidden-phone hidden-tablet">
+			<li><a href="<? echo base_url()?>admin/products">Products</a></li>
+			<li><a href="<? echo base_url()?>admin/categories">Categories</a></li>
+			<li><a href="<? echo base_url()?>"><img src="<? echo base_url()?>images/Logo.jpg" class="logo" width="150px;"></a></li>
+			<li><a href="<? echo base_url()?>admin/subcategories">Subcategories</a></li>
+			<li><a href="<? echo base_url()?>admin/users">Utilizatori</a></li>
+		</ul>
+      </div>
+	</footer>
+</body>
+</html>
---
+++ Web Database Management/application/views/json_view.php
@@ -0,0 +1,4 @@
+<?php   
+  $this->output->set_header('Content-Type: application/json; charset=utf-8');
+  echo json_encode($json);
+?>
